65741866da293174c01b048b1246594f6d0470c3
GiuliaGallorini <giuliagallorini7@gmail.com>
2019-06-17 15:54:01 +0100

    Created folder images. Created database.js with arrayFruitsAndVegs of objects

---
+++ js/database.js
@@ -0,0 +1,8 @@
+const arrayFruitsAndVegs = [
+    {name: "apple", missingPlu: "200"},
+    {name: "banana", missingPlu: "210"},
+    {name: "strawberry", missingPlu: "220"},
+    {name: "zucchini", missingPlu: "300"},
+    {name: "tomato", missingPlu: "310"},
+    {name: "salad", missingPlu: "320"}
+];
